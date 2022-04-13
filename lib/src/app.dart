d4b8bf35d737347040183b15fb009a5c8eddc630
skedaddl3 <hexisdylan23@gmail.com>
2022-04-13 11:00:46 +0800

    added statefulwidget

---
+++ lib/src/app.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class App extends StatefulWidget {
+  createState() {
+    return AppState();
+  }
+}
+
+class AppState extends State<App> {
+  int counter = 0;
+
+  @override
+  Widget build(context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Text('$counter'),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: () {
+            setState(() {
+              counter += 1;
+            });
+          },
+        ),
+        appBar: AppBar(
+          title: Text('Lets see some images'),
+        ),
+      ),
+    );
+  }
+}
